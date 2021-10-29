cc1424514f0fc79805f08b9eeb782749cb7ca3b6
Robyn <robyn.g@hotmail.fr>
2021-10-29 00:47:53 -0400

    Remove dependencies from lapis and extract MapIO miniproject for map files

--- ebi/cores/Sapphire Owl/src/com/souchy/randd/ebishoal/sapphire/main/SapphireOwl.java
+++ ebi/cores/Sapphire Owl/src/com/souchy/randd/ebishoal/sapphire/main/SapphireOwl.java
@@ -7,13 +7,14 @@ import com.souchy.jeffekseer.FXLoader;
 import com.souchy.randd.commons.deathebi.msg.GetSalt;
 import com.souchy.randd.commons.tealwaters.io.files.JsonConfig;
 import com.souchy.randd.commons.tealwaters.logging.Log;
+import com.souchy.randd.ebishoal.commons.EbiShoalCore;
 import com.souchy.randd.ebishoal.commons.lapis.main.LapisCore;
 import com.souchy.randd.ebishoal.commons.lapis.main.LapisGame;
 import com.souchy.randd.ebishoal.sapphire.confs.SapphireOwlConf;
 import com.souchy.randd.ebishoal.sapphire.controls.Commands;
 import com.souchy.randd.moonstone.white.Moonstone;
 
-public class SapphireOwl extends LapisCore { 
+public class SapphireOwl extends EbiShoalCore { 
 
 
 	/**
@@ -61,49 +62,60 @@ public class SapphireOwl extends LapisCore {
 		}
 		Log.info("******************************************************************************");
 		
-		core.start();
+
+		
+		new SapphireOwl(args);
 	}
 
 	public SapphireOwl(String[] args) throws Exception {
 		super(args);
+		new SapphireLapis(args).start();
 	}
 	
-	@Override
-	public void init() throws Exception {
-		super.init();
-		// load sapphire config
-		conf = JsonConfig.readExternal(SapphireOwlConf.class, "./modules/");
-		
-		// TODO À l'avenir, on aura un fichier qui map les ressources aux creatureID, spellID, statusID
-		// ex CreatureResources : {creatureID} : { "modelpath" }
-		// ex SpellResources : {spellid} : {spellIcon}, {spellFX}
-		// ex StatusResources : {statusid} : {statusIcon}, {status9patch}, {statusCenterCell} // 9patch et centercell pour les terrainstatus 
-		// i18n/francais/spells : {spellid} : { name:"", description:"" } 
-		// i18n/francais/statuses : {statusid} : { name:"", description:"" } 
-		// peut-être aussi un fichier resources pour des presets de modèles/voxels à utiliser dans les fichiers de maps
-		// 		pour les rendre global
-		
-	}
 	
-	@Override
-	protected LapisGame createGame() {
-		return game;
-	}
-
 	@Override
 	protected String[] getRootPackages(){
 		return new String[] { "com.souchy.randd.commons.deathebi.msg", "com.souchy.randd.moonstone", "com.souchy.randd.moonstone.white", "com.souchy.randd.ebishoal.sapphire" };
 	}
 	
-	@Override
-	public SapphireGame getGame() {
-		return game;
+	
+	private class SapphireLapis extends LapisCore {
+		public SapphireLapis(String[] args) throws Exception {
+			super(args);
+		}
+		@Override
+		public void init() throws Exception {
+			super.init();
+			// load sapphire config
+			conf = JsonConfig.readExternal(SapphireOwlConf.class, "./modules/");
+			
+			// TODO À l'avenir, on aura un fichier qui map les ressources aux creatureID, spellID, statusID
+			// ex CreatureResources : {creatureID} : { "modelpath" }
+			// ex SpellResources : {spellid} : {spellIcon}, {spellFX}
+			// ex StatusResources : {statusid} : {statusIcon}, {status9patch}, {statusCenterCell} // 9patch et centercell pour les terrainstatus 
+			// i18n/francais/spells : {spellid} : { name:"", description:"" } 
+			// i18n/francais/statuses : {statusid} : { name:"", description:"" } 
+			// peut-être aussi un fichier resources pour des presets de modèles/voxels à utiliser dans les fichiers de maps
+			// 		pour les rendre global
+			
+		}
+		
+		@Override
+		protected LapisGame createGame() {
+			return game;
+		}
+	
+		
+		@Override
+		public SapphireGame getGame() {
+			return game;
+		}
+	
+		@Override
+		public void addIcon(LwjglApplicationConfiguration config) {
+			config.addIcon("res/textures/appicon4.png", FileType.Internal); //.addIcon("G:/Assets/pack/fantasy bundle/tcgcardspack/Tex_krakken_icon.png", FileType.Absolute);
+		}
 	}
-
-	@Override
-	public void addIcon(LwjglApplicationConfiguration config) {
-		config.addIcon("res/textures/appicon4.png", FileType.Internal); //.addIcon("G:/Assets/pack/fantasy bundle/tcgcardspack/Tex_krakken_icon.png", FileType.Absolute);
-	}
-
+	
 	
 }
