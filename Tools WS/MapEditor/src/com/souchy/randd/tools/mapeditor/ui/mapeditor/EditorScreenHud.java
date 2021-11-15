d9eeeb823be3f39b26b7524a64fc98c9707e59c2
Robyn <robyn.g@hotmail.fr>
2021-11-14 19:23:33 -0500

    Hud.open/save() filechooser

--- Tools WS/MapEditor/src/com/souchy/randd/tools/mapeditor/ui/mapeditor/EditorScreenHud.java
+++ Tools WS/MapEditor/src/com/souchy/randd/tools/mapeditor/ui/mapeditor/EditorScreenHud.java
@@ -1,17 +1,31 @@
 package com.souchy.randd.tools.mapeditor.ui.mapeditor;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.kotcrab.vis.ui.widget.VisTable;
+import com.kotcrab.vis.ui.widget.file.FileChooser;
+import com.kotcrab.vis.ui.widget.file.FileChooserAdapter;
+import com.kotcrab.vis.ui.widget.file.FileTypeFilter;
+import com.kotcrab.vis.ui.widget.file.FileChooser.Mode;
+import com.kotcrab.vis.ui.widget.file.FileChooser.SelectionMode;
 import com.souchy.randd.commons.tealwaters.logging.Log;
 import com.souchy.randd.ebishoal.commons.lapis.gfx.screen.LapisHud;
 import com.souchy.randd.ebishoal.commons.lapis.gfx.shaders.LapisShader;
+import com.souchy.randd.tools.mapeditor.main.MapEditorCore;
+import com.souchy.randd.tools.mapeditor.main.MapEditorGame;
 import com.souchy.randd.tools.mapeditor.ui.components.BottomBar;
 import com.souchy.randd.tools.mapeditor.ui.components.Component;
 import com.souchy.randd.tools.mapeditor.ui.components.PropertiesPanel;
@@ -88,6 +102,66 @@ public class EditorScreenHud extends LapisHud {
 	@Override
 	public String getViewId() {
 		return "editor";
+	}
+	
+
+	public void open(Path folder, String extension, Consumer<Array<FileHandle>> onSelect) {
+		open(folder.toAbsolutePath().toString(), extension, onSelect);
+	}
+	public void open(String folder, String extension, Consumer<Array<FileHandle>> onSelect) {
+		FileChooser fileChooser = new FileChooser(Mode.OPEN);
+		fileChooser.setSelectionMode(SelectionMode.FILES);
+		fileChooser.setMultiSelectionEnabled(true);
+		fileChooser.setDirectory(Gdx.files.internal(folder).file()); // data/maps/
+//		fileChooser.setFileFilter(new FileFilter() {
+//			@Override
+//			public boolean accept(File file) {
+//				return file.isDirectory() || file.getName().endsWith(extension);
+//			}
+//		});
+		if(extension != null && !extension.isEmpty()) {
+			var filter = new FileTypeFilter(true);
+			filter.addRule(extension, extension);
+			fileChooser.setFileTypeFilter(filter);
+		}
+		
+		
+		fileChooser.setListener(new FileChooserAdapter() {
+			@Override
+			public void selected(Array<FileHandle> files) {
+				super.selected(files);
+				onSelect.accept(files);
+			}
+		});
+		this.getStage().addActor(fileChooser.fadeIn());
+	}
+
+	public void saveAs(Path folder, String extension, Consumer<Array<FileHandle>> onSelect) {
+		saveAs(folder.toAbsolutePath().toString(), extension, onSelect);
+	}
+	public void saveAs(String folder, String extension, Consumer<Array<FileHandle>> onSelect) {
+		FileChooser fileChooser = new FileChooser(Mode.SAVE);
+		fileChooser.setSelectionMode(SelectionMode.FILES);
+		fileChooser.setDirectory(Gdx.files.internal(folder).file());
+//		fileChooser.setFileFilter(new FileFilter() {
+//			@Override
+//			public boolean accept(File file) {
+//				return file.isDirectory() || file.getName().endsWith(extension);
+//			}
+//		});
+		if(extension != null && !extension.isEmpty()) {
+			var filter = new FileTypeFilter(true);
+			filter.addRule(extension, extension);
+			fileChooser.setFileTypeFilter(filter);
+		}
+		
+		fileChooser.setListener(new FileChooserAdapter() {
+			@Override
+			public void selected (Array<FileHandle> files) {
+				onSelect.accept(files);
+			}
+		});
+		this.getStage().addActor(fileChooser.fadeIn());
 	}
 	
 	
